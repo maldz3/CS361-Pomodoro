5d4f62cf3f351738049ade86a363164421494c09
Jamie Mott <jamie.c.mott@gmail.com>
2020-07-11 11:04:55 -0600

    Centered tab bar title

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'build_tabs.dart';
 
 class App extends StatelessWidget {
-
   final String title;
 
   const App({Key key, this.title}) : super(key: key);
@@ -10,16 +9,15 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: title,
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: DefaultTabController(
-        length: 3,
-        initialIndex: 0,
-        child: BuildTabs(title: 'Pomodoro'),
-      )
-    );
+        title: title,
+        theme: ThemeData(
+          primarySwatch: Colors.blueGrey,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: DefaultTabController(
+          length: 3,
+          initialIndex: 0,
+          child: BuildTabs(title: title),
+        ));
   }
 }
