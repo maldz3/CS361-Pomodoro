332acd3dcba2ddb53dcfb446abbd6fef80a014c0
Jamie Mott <jamie.c.mott@gmail.com>
2020-07-11 10:33:08 -0600

    Added simple UI

--- lib/account_page.dart
+++ lib/account_page.dart
@@ -1,28 +1,46 @@
 import 'package:flutter/material.dart';
 
 class AccountPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        pageTitle()
+        pageTitle(),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            rowBuilder('Username: '),
+            rowBuilder('Email: '),
+            FlatButton(child: Text('Change Password'), onPressed: () {}),
+            FlatButton(
+                color: Colors.red,
+                textColor: Colors.white,
+                child: Text('Remove Account Data'),
+                onPressed: () {})
+          ],
+        )
       ],
     );
   }
 
   Widget pageTitle() {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Text(
-        'Account Page',
-        style: TextStyle(
-          fontSize: 25.0,
-          height: 2.0
-        )
-      )
-    );
+        padding: const EdgeInsets.all(8.0),
+        child: Text('Account Page',
+            style: TextStyle(fontSize: 25.0, height: 2.0)));
   }
 
+  Widget rowBuilder(String title) {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      Text(title),
+      Text('Placeholder'),
+      FlatButton(
+          color: Colors.blueGrey,
+          textColor: Colors.white,
+          padding: EdgeInsets.all(8.0),
+          child: Text('Update'),
+          onPressed: () {})
+    ]);
+  }
 }
