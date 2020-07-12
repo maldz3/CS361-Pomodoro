ecd645bb336bed4e92b11596431cc5d9916c2f6b
Jamie Mott <jamie.c.mott@gmail.com>
2020-07-12 08:27:15 -0600

    Added routes for all buttons

--- lib/account_page.dart
+++ lib/account_page.dart
@@ -13,14 +13,20 @@ class AccountPage extends StatelessWidget {
         Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            rowBuilder('Username: ', 'Email :'),
-            FlatButton(child: Text('Change Password'), onPressed: () {}),
+            rowBuilder('Username: ', 'Email :', context),
+            FlatButton(
+                child: Text('Change Password'),
+                onPressed: () {
+                  changePassword(context);
+                }),
             FlatButton(
                 color: Colors.red,
                 textColor: Colors.white,
                 splashColor: Colors.redAccent,
                 child: Text('Remove Account Data'),
-                onPressed: () {})
+                onPressed: () {
+                  deleteData(context);
+                })
           ],
         )
       ],
@@ -34,7 +40,7 @@ class AccountPage extends StatelessWidget {
             style: TextStyle(fontSize: 25.0, height: 2.0)));
   }
 
-  Widget rowBuilder(String title1, String title2) {
+  Widget rowBuilder(String title1, String title2, BuildContext context) {
     return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
       Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -59,15 +65,91 @@ class AccountPage extends StatelessWidget {
             splashColor: Colors.blueAccent,
             padding: EdgeInsets.all(8.0),
             child: Text('Update'),
-            onPressed: () {}),
+            onPressed: () {
+              changeName(context);
+            }),
         FlatButton(
             color: Colors.blueGrey,
             textColor: Colors.white,
             splashColor: Colors.blueAccent,
             padding: EdgeInsets.all(8.0),
             child: Text('Update'),
-            onPressed: () {})
+            onPressed: () {
+              changeEmail(context);
+            })
       ]),
     ]);
+  }
+
+  void changeName(BuildContext context) {
+    Navigator.push(context, MaterialPageRoute<void>(
+      builder: (BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(title: Text('Update Username')),
+          body: Center(
+            child: FlatButton(
+              child: Text('Click here to return'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        );
+      },
+    ));
+  }
+
+  void changeEmail(BuildContext context) {
+    Navigator.push(context, MaterialPageRoute<void>(
+      builder: (BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(title: Text('Update Email')),
+          body: Center(
+            child: FlatButton(
+              child: Text('Click here to return'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        );
+      },
+    ));
+  }
+
+  void changePassword(BuildContext context) {
+    Navigator.push(context, MaterialPageRoute<void>(
+      builder: (BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(title: Text('Change Password')),
+          body: Center(
+            child: FlatButton(
+              child: Text('Click here to return'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        );
+      },
+    ));
+  }
+
+  void deleteData(BuildContext context) {
+    Navigator.push(context, MaterialPageRoute<void>(
+      builder: (BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(title: Text('Remove Account Data')),
+          body: Center(
+            child: FlatButton(
+              child: Text('Click here to return'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        );
+      },
+    ));
   }
 }
