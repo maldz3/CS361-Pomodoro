2e269b9fe8138f79fa0998daa7a3c9bda97bacc9
Leonard LoBiondo <llobiondo2@gmail.com>
2020-07-16 10:47:37 -0700

    Custom app bar added to timer page.  Password, email, and bad constructor removed from user object

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -6,19 +6,9 @@ class User {
   FirebaseUser firebaseUser;
   String uid;
   String username;
-  String email;
-  String password;
   int level;
   List<Task> tasks;
   List<Category> categories;
-
-  User(String username, String email, String password) {
-    this.username = username;
-    this.email = email;
-    this.level = 1;
-    tasks = [];
-    categories = [];
-  }
 
   User.fromFirebaseUser(FirebaseUser user, String uid){
     this.firebaseUser = user;
@@ -31,14 +21,6 @@ class User {
 
   void changeName(String newName) {
     username = newName;
-  }
-
-  void changeEmail(String newEmail) {
-    email = newEmail;
-  }
-
-  void changePwd(String newPwd) {
-    password = newPwd;
   }
 
 }
