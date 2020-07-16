44184e8edf36233553bc1a4eab04aa378e2d31c5
Jamie Mott <jamie.c.mott@gmail.com>
2020-07-15 19:32:25 -0600

    added routes to settings and timer page

--- lib/app.dart
+++ lib/app.dart
@@ -15,16 +15,18 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamProvider<User>.value(
-      value: AuthService().userAuth,
-      child: MaterialApp(
-        title: title,
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blueGrey,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: RootPage() 
-      )    
-    );
+        value: AuthService().userAuth,
+        child: MaterialApp(
+            title: title,
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              primarySwatch: Colors.blueGrey,
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
+            home: RootPage(),
+            routes: {
+              '/settings': (context) => AccountPage(),
+              '/timer': (context) => TimerPage()
+            }));
   }
 }
