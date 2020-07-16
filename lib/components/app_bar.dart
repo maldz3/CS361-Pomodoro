784809db05958b9f1d310b34f83f03c2679e1b61
Leonard LoBiondo <llobiondo2@gmail.com>
2020-07-16 09:33:48 -0700

    custom app bar has logout functionality with logout button

--- lib/components/app_bar.dart
+++ lib/components/app_bar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:pomodoro/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:pomodoro/root_page.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   String title;
@@ -21,7 +22,11 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
               label: Text("logout"),
               onPressed: () async {
                 await _auth.signOut();
-              })
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => RootPage()));
+                
+              }
+          )
         ]
         /*Padding(
             padding: EdgeInsets.only(right: 10),
