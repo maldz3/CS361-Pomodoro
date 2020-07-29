fae9b1634f9a321a6817ee461bdf17137ec03f14
Leonard LoBiondo <llobiondo2@gmail.com>
2020-07-28 17:23:19 -0700

    Account page redo is up and functional.  Added padding to task add form, and added username to top of task list

--- lib/account_page.dart
+++ lib/account_page.dart
@@ -33,123 +33,129 @@ class _AccountPageState extends State<AccountPage> {
         body: Container(
           child: Column(
             children: [
-              Container(
-                padding: const EdgeInsets.all(12),
-                color: Colors.blueGrey[200],
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                      children: [
-                        smallHeader(context, 'User Name'),
-                        sortaBigText(context, '$username')
-                      ]
-                    ),
-                    Column(
-                      children: [
-                        smallHeader(context, 'Email'),
-                        sortaBigText(context, '$email')
-                      ],
-                    )
-                ]),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: FractionallySizedBox(
-                  widthFactor: .6,
-                  child: Form(
-                    key: userKey,
-                    autovalidate: userAutoValidate,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Expanded(
-                          child: TextFormField(
-                            maxLength: 10,
-                            decoration: InputDecoration(
-                              labelText: "New Username", 
-                              border: OutlineInputBorder(),
-                              counterText: ""
-                            ),
-                            onSaved: (value) {
-                              setState(() {
-                                username = value;
-                              });
-                              this.widget.user.changeName(value);
-                              //store it
-                            },
-                            validator: (value) => value.isEmpty ? "Enter a new username" : null
-                          ),
-                        ),
-                        SizedBox(width: 20),
-                        RaisedButton(
-                          onPressed: () {
-                            if (userKey.currentState.validate()) {
-                              userKey.currentState.save();
-                            } else {
-                              setState(() {
-                                userAutoValidate = true;
-                              }); 
-                            }
-                          },
-                          child: Text('Update')
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: FractionallySizedBox(
-                  widthFactor: .6,
-                  child: Form(
-                    key: emailKey,
-                    autovalidate: emailAutoValidate,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Expanded(
-                          child: TextFormField(
-                            maxLength: 30,
-                              decoration: InputDecoration(
-                                labelText: "New Email", 
-                                border: OutlineInputBorder(),
-                                counterText: ""
-                              ),
-                              onSaved: (value) {
-                                setState(() {
-                                  email = value;
-                                });
-                                this.widget.user.changeEmail(value);
-                                //store it
-                              },
-                              validator: (value) => !EmailValidator.validate(value) ? "Enter a valid email" : null
-                            ),
-                          ),
-                          SizedBox(width: 20),
-                          RaisedButton(
-                            onPressed: () {
-                              if (emailKey.currentState.validate()) {
-                                emailKey.currentState.save();
-                              } else {
-                                setState(() {
-                                  emailAutoValidate = true;
-                                }); 
-                              }
-                            },
-                            child: Text('Update')
-                          ),
-                      ],
-                    )
-                  ),
-                ),
-              ),
+              accountInfoHeader(username, email),
+              usernameUpdateRow(username),
+              emailUpdateRow(email),
             ]
         ),
       ),
     );
   }
 
+  Widget accountInfoHeader(String username, String email) {
+    return Container(
+      padding: const EdgeInsets.all(12),
+      color: Colors.blueGrey[200],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          headerColumn('User Name', '$username'),
+          headerColumn('Email', '$email')
+      ]),
+    );
+  }
 
+  Widget headerColumn(String heading, String value) {
+    return Column(
+      children: [
+        smallHeader(context, heading),
+        sortaBigText(context, value)
+      ]
+    );
+  }
+
+  Widget usernameUpdateRow(String username) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0),
+      child: FractionallySizedBox(
+        widthFactor: .6,
+        child: Form(
+          key: userKey,
+          autovalidate: userAutoValidate,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: TextFormField(
+                  maxLength: 10,
+                  decoration: InputDecoration(
+                    labelText: "New Username", 
+                    border: OutlineInputBorder(),
+                    counterText: ""
+                  ),
+                  onSaved: (value) {
+                    setState(() {
+                      username = value;
+                    });
+                    this.widget.user.changeName(value);
+                    //store it
+                  },
+                  validator: (value) => value.isEmpty ? "Enter a new username" : null
+                ),
+              ),
+              SizedBox(width: 20),
+              updateButton(userKey, 'user')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget emailUpdateRow(String email) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0),
+      child: FractionallySizedBox(
+        widthFactor: .6,
+        child: Form(
+          key: emailKey,
+          autovalidate: emailAutoValidate,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: TextFormField(
+                  maxLength: 30,
+                    decoration: InputDecoration(
+                      labelText: "New Email", 
+                      border: OutlineInputBorder(),
+                      counterText: ""
+                    ),
+                    onSaved: (value) {
+                      setState(() {
+                        email = value;
+                      });
+                      this.widget.user.changeEmail(value);
+                      //store it
+                    },
+                    validator: (value) => !EmailValidator.validate(value) ? "Enter a valid email" : null
+                  ),
+                ),
+                SizedBox(width: 20),
+                updateButton(emailKey, 'email')
+            ],
+          )
+        ),
+      ),
+    );
+  }
+
+  Widget updateButton(GlobalKey<FormState> key, String type) {
+    return RaisedButton(
+      onPressed: () {
+        if (key.currentState.validate()) {
+          key.currentState.save();
+          key.currentState.reset();
+        if (type == 'email') emailAutoValidate = false;
+        else userAutoValidate = false;
+        } else {
+          setState(() {
+            if (type == 'email') emailAutoValidate = true;
+            else userAutoValidate = true;
+          }); 
+        }
+      },
+      child: Text('Update')
+    );
+  }
 }
--- lib/tasks_add_page.dart
+++ lib/tasks_add_page.dart
@@ -20,163 +20,185 @@ class _TasksAddPageState extends State<TasksAddPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: CustomAppBar('Add a Task', this.widget.user),
-        body: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Form(
-                key: formKey,
-                child: SingleChildScrollView(
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                      TextFormField(
-                          autofocus: true,
-                          decoration: InputDecoration(
-                              labelText: 'Task Name',
-                              border: OutlineInputBorder()),
-                          onSaved: (value) {
-                            newTask.name = value;
-                          },
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Please enter a task name';
-                            } else {
-                              return null;
-                            }
-                          }),
-                      TextFormField(
-                          autofocus: true,
-                          decoration: InputDecoration(
-                              labelText: 'Description',
-                              border: OutlineInputBorder()),
-                          onSaved: (value) {
-                            newTask.description = value;
-                          },
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Please enter a description';
-                            } else {
-                              return null;
-                            }
-                          }),
-                      TextFormField(
-                          autofocus: true,
-                          inputFormatters: [
-                            FilteringTextInputFormatter.digitsOnly
-                          ],
-                          decoration: InputDecoration(
-                              labelText: 'Task Duration (Minutes)',
-                              border: OutlineInputBorder()),
-                          onSaved: (value) {
-                            newTask.durationWork = int.tryParse(value);
-                          },
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Please enter a task duration';
-                            } else {
-                              return null;
-                            }
-                          }),
-                      TextFormField(
-                          autofocus: true,
-                          inputFormatters: [
-                            FilteringTextInputFormatter.digitsOnly
-                          ],
-                          decoration: InputDecoration(
-                              labelText: 'Break Duration (Minutes)',
-                              border: OutlineInputBorder()),
-                          onSaved: (value) {
-                            newTask.durationBreak = int.tryParse(value);
-                          },
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Please enter a break duration';
-                            } else {
-                              return null;
-                            }
-                          }),
-                      TextFormField(
-                          autofocus: true,
-                          inputFormatters: [
-                            FilteringTextInputFormatter.digitsOnly
-                          ],
-                          decoration: InputDecoration(
-                              labelText: 'Goal Time (Minutes)',
-                              border: OutlineInputBorder()),
-                          onSaved: (value) {
-                            newTask.goalTime = int.tryParse(value);
-                          },
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Please enter a goal time';
-                            } else {
-                              return null;
-                            }
-                          }),
-                      DropDownFormField(
-                        titleText: 'Category',
-                        hintText: 'Please choose one',
-                        value: newTask.category,
-                        onSaved: (value) {
-                          setState(() {
-                            newTask.category = value;
-                          });
+      appBar: CustomAppBar('Add a Task', this.widget.user),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Form(
+          key: formKey,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    autofocus: true,
+                    decoration: InputDecoration(
+                        labelText: 'Task Name',
+                        border: OutlineInputBorder()),
+                    onSaved: (value) {
+                      newTask.name = value;
+                    },
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter a task name';
+                      } else {
+                        return null;
+                      }
+                    }
+                  ),
+                  SizedBox(height: 8),    
+                  TextFormField(
+                    autofocus: true,
+                    decoration: InputDecoration(
+                      labelText: 'Description',
+                      border: OutlineInputBorder()),
+                    onSaved: (value) {
+                      newTask.description = value;
+                    },
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter a description';
+                      } else {
+                        return null;
+                      }
+                    }
+                  ),
+                  SizedBox(height: 8),
+                  TextFormField(
+                    autofocus: true,
+                    inputFormatters: [
+                    FilteringTextInputFormatter.digitsOnly
+                    ],
+                    decoration: InputDecoration(
+                      labelText: 'Task Duration (Minutes)',
+                      border: OutlineInputBorder()
+                    ),
+                    onSaved: (value) {
+                      newTask.durationWork = int.tryParse(value);
+                    },
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter a task duration';
+                      } else {
+                        return null;
+                      }
+                    }
+                  ),
+                  SizedBox(height: 8),
+                  TextFormField(
+                    autofocus: true,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
+                    decoration: InputDecoration(
+                        labelText: 'Break Duration (Minutes)',
+                        border: OutlineInputBorder()),
+                    onSaved: (value) {
+                      newTask.durationBreak = int.tryParse(value);
+                    },
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter a break duration';
+                      } else {
+                        return null;
+                      }
+                    }
+                  ),
+                  SizedBox(height: 8),
+                  TextFormField(
+                    autofocus: true,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
+                    decoration: InputDecoration(
+                        labelText: 'Goal Time (Minutes)',
+                        border: OutlineInputBorder()),
+                    onSaved: (value) {
+                      newTask.goalTime = int.tryParse(value);
+                    },
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter a goal time';
+                      } else {
+                        return null;
+                      }
+                    }
+                  ),
+                  SizedBox(height: 8),
+                  DropDownFormField(
+                    titleText: 'Category',
+                    hintText: 'Please choose one',
+                    value: newTask.category,
+                    onSaved: (value) {
+                      setState(() {
+                        newTask.category = value;
+                      });
+                    },
+                    validator: (value) {
+                      if (value == null) {
+                        return 'Please select a category';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onChanged: (value) {
+                      setState(() {
+                        newTask.category = value;
+                      });
+                    },
+                    dataSource: [
+                      {'display': 'School', 'value': 'School'},
+                      {'display': 'Work', 'value': 'Work'},
+                      {'display': 'Exercise', 'value': 'Exercise'},
+                      {'display': 'Home', 'value': 'Home'},
+                      {'display': 'Family', 'value': 'Family'},
+                      {'display': 'Other', 'value': 'Other'}
+                    ],
+                    textField: 'display',
+                    valueField: 'value',
+                  ),
+                  SizedBox(height: 20.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      RaisedButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
                         },
-                        validator: (value) {
-                          if (value == null) {
-                            return 'Please select a category';
-                          } else {
-                            return null;
+                        child: Text('Cancel')
+                      ),
+                      RaisedButton(
+                        color: Colors.red,
+                        onPressed: () async {
+                          if (formKey.currentState.validate()) {
+                            formKey.currentState.save();
+                            this.widget.user = ModalRoute.of(context)
+                                .settings
+                                .arguments;
+                            User user = this.widget.user;
+                            Task addTask = Task(
+                                name: newTask.name,
+                                description: newTask.description,
+                                durationWork: newTask.durationWork,
+                                durationBreak: newTask.durationBreak,
+                                goalTime: newTask.goalTime,
+                                category: newTask.category);
+
+                            user.tasks.add(addTask);
+                            Navigator.of(context).pop();
                           }
                         },
-                        onChanged: (value) {
-                          setState(() {
-                            newTask.category = value;
-                          });
-                        },
-                        dataSource: [
-                          {'display': 'School', 'value': 'School'},
-                          {'display': 'Work', 'value': 'Work'},
-                          {'display': 'Exercise', 'value': 'Exercise'},
-                          {'display': 'Home', 'value': 'Home'},
-                          {'display': 'Family', 'value': 'Family'},
-                          {'display': 'Other', 'value': 'Other'}
-                        ],
-                        textField: 'display',
-                        valueField: 'value',
-                      ),
-                      SizedBox(height: 20.0),
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            RaisedButton(
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                                child: Text('Cancel')),
-                            RaisedButton(
-                                color: Colors.red,
-                                onPressed: () async {
-                                  if (formKey.currentState.validate()) {
-                                    formKey.currentState.save();
-                                    this.widget.user = ModalRoute.of(context)
-                                        .settings
-                                        .arguments;
-                                    User user = this.widget.user;
-                                    Task addTask = Task(
-                                        name: newTask.name,
-                                        description: newTask.description,
-                                        durationWork: newTask.durationWork,
-                                        durationBreak: newTask.durationBreak,
-                                        goalTime: newTask.goalTime,
-                                        category: newTask.category);
-
-                                    user.tasks.add(addTask);
-                                    Navigator.of(context).pop();
-                                  }
-                                },
-                                child: Text('SaveEntry'))
-                          ])
-                    ])))));
+                        child: Text('SaveEntry')
+                      )
+                    ]
+                  )
+                ]
+              ),
+            )
+          )
+        )
+      )
+    );
   }
 }
