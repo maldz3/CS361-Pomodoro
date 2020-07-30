bfa99bf918632eaaec9a0197f14b404733bcfdd6
Jamie Mott <jamie.c.mott@gmail.com>
2020-07-29 22:12:48 -0600

    Added text to alert dialog.
    IDE also seems to have auto formatted most of the file.

--- lib/tasks_add_page.dart
+++ lib/tasks_add_page.dart
@@ -9,7 +9,7 @@ import 'package:pomodoro/models/task_dto.dart';
 class TaskAddPageArgs {
   User user;
   Task task;
-  TaskAddPageArgs(this.user, {Task task}): task = task;
+  TaskAddPageArgs(this.user, {Task task}) : task = task;
 }
 
 //New Task Entry form
@@ -39,180 +39,175 @@ class _TasksAddPageState extends State<TasksAddPage> {
       taskInfo.id = taskToEdit.id;
     }
     return Scaffold(
-      appBar: CustomAppBar(title, user),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Form(
-          key: formKey,
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextFormField(
-                    initialValue: (taskToEdit == null)? null : taskToEdit.name,
-                    autofocus: true,
-                    decoration: InputDecoration(
-                        labelText: 'Task Name',
-                        border: OutlineInputBorder()),
-                    onSaved: (value) {
-                      taskInfo.name = value;
-                    },
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter a task name';
-                      } else {
-                        return null;
-                      }
-                    }
-                  ),
-                  SizedBox(height: 8),    
-                  TextFormField(
-                      initialValue: (taskToEdit == null)? null : taskToEdit.description,
-                    autofocus: true,
-                    decoration: InputDecoration(
-                      labelText: 'Description',
-                      border: OutlineInputBorder()),
-                    onSaved: (value) {
-                      taskInfo.description = value;
-                    },
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter a description';
-                      } else {
-                        return null;
-                      }
-                    }
-                  ),
-                  SizedBox(height: 8),
-                  TextFormField(
-                      initialValue: (taskToEdit == null)? null : taskToEdit.durationWork.toString(),
-                    autofocus: true,
-                    inputFormatters: [
-                    FilteringTextInputFormatter.digitsOnly
-                    ],
-                    decoration: InputDecoration(
-                      labelText: 'Task Duration (Minutes)',
-                      border: OutlineInputBorder()
-                    ),
-                    onSaved: (value) {
-                      taskInfo.durationWork = int.tryParse(value);
-                    },
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter a task duration';
-                      } else {
-                        return null;
-                      }
-                    }
-                  ),
-                  SizedBox(height: 8),
-                  TextFormField(
-                      initialValue: (taskToEdit == null)? null : taskToEdit.durationBreak.toString(),
-                    autofocus: true,
-                    inputFormatters: [
-                      FilteringTextInputFormatter.digitsOnly
-                    ],
-                    decoration: InputDecoration(
-                        labelText: 'Break Duration (Minutes)',
-                        border: OutlineInputBorder()),
-                    onSaved: (value) {
-                      taskInfo.durationBreak = int.tryParse(value);
-                    },
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter a break duration';
-                      } else {
-                        return null;
-                      }
-                    }
-                  ),
-                  SizedBox(height: 8),
-                  TextFormField(
-                      initialValue: (taskToEdit == null)? null : taskToEdit.goalTime.toString(),
-                    autofocus: true,
-                    inputFormatters: [
-                      FilteringTextInputFormatter.digitsOnly
-                    ],
-                    decoration: InputDecoration(
-                        labelText: 'Goal Time (Minutes)',
-                        border: OutlineInputBorder()),
-                    onSaved: (value) {
-                      taskInfo.goalTime = int.tryParse(value);
-                    },
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter a goal time';
-                      } else {
-                        return null;
-                      }
-                    }
-                  ),
-                  SizedBox(height: 8),
-                  DropDownFormField(
-                    titleText: 'Category',
-                    hintText: 'Please choose one',
-                    value: taskInfo.category,
-                    onSaved: (value) {
-                      setState(() {
-                        taskInfo.category = value;
-                      });
-                    },
-                    validator: (value) {
-                      if (value == null) {
-                        return 'Please select a category';
-                      } else {
-                        return null;
-                      }
-                    },
-                    onChanged: (value) {
-                      setState(() {
-                        taskInfo.category = value;
-                      });
-                    },
-                    dataSource: user.tasks.categories,
-                    textField: 'id',
-                    valueField: 'value',
-                  ),
-                  SizedBox(height: 20.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      RaisedButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: Text('Cancel')
-                      ),
-                      RaisedButton(
-                        color: Colors.red,
-                        onPressed: () async {
-                          if (formKey.currentState.validate()) {
-                            formKey.currentState.save();
-                            Task newTask = Task(
-                                id: taskInfo.id,
-                                name: taskInfo.name,
-                                description: taskInfo.description,
-                                durationWork: taskInfo.durationWork,
-                                durationBreak: taskInfo.durationBreak,
-                                goalTime: taskInfo.goalTime,
-                                category: taskInfo.category);
+        appBar: CustomAppBar(title, user),
+        body: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Form(
+                key: formKey,
+                child: SingleChildScrollView(
+                    child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextFormField(
+                            initialValue:
+                                (taskToEdit == null) ? null : taskToEdit.name,
+                            autofocus: true,
+                            decoration: InputDecoration(
+                                labelText: 'Task Name',
+                                border: OutlineInputBorder()),
+                            onSaved: (value) {
+                              taskInfo.name = value;
+                            },
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please enter a task name';
+                              } else {
+                                return null;
+                              }
+                            }),
+                        SizedBox(height: 8),
+                        TextFormField(
+                            initialValue: (taskToEdit == null)
+                                ? null
+                                : taskToEdit.description,
+                            autofocus: true,
+                            decoration: InputDecoration(
+                                labelText: 'Description',
+                                border: OutlineInputBorder()),
+                            onSaved: (value) {
+                              taskInfo.description = value;
+                            },
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please enter a description';
+                              } else {
+                                return null;
+                              }
+                            }),
+                        SizedBox(height: 8),
+                        TextFormField(
+                            initialValue: (taskToEdit == null)
+                                ? null
+                                : taskToEdit.durationWork.toString(),
+                            autofocus: true,
+                            inputFormatters: [
+                              FilteringTextInputFormatter.digitsOnly
+                            ],
+                            decoration: InputDecoration(
+                                labelText: 'Task Duration (Minutes)',
+                                border: OutlineInputBorder()),
+                            onSaved: (value) {
+                              taskInfo.durationWork = int.tryParse(value);
+                            },
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please enter a task duration';
+                              } else {
+                                return null;
+                              }
+                            }),
+                        SizedBox(height: 8),
+                        TextFormField(
+                            initialValue: (taskToEdit == null)
+                                ? null
+                                : taskToEdit.durationBreak.toString(),
+                            autofocus: true,
+                            inputFormatters: [
+                              FilteringTextInputFormatter.digitsOnly
+                            ],
+                            decoration: InputDecoration(
+                                labelText: 'Break Duration (Minutes)',
+                                border: OutlineInputBorder()),
+                            onSaved: (value) {
+                              taskInfo.durationBreak = int.tryParse(value);
+                            },
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please enter a break duration';
+                              } else {
+                                return null;
+                              }
+                            }),
+                        SizedBox(height: 8),
+                        TextFormField(
+                            initialValue: (taskToEdit == null)
+                                ? null
+                                : taskToEdit.goalTime.toString(),
+                            autofocus: true,
+                            inputFormatters: [
+                              FilteringTextInputFormatter.digitsOnly
+                            ],
+                            decoration: InputDecoration(
+                                labelText: 'Goal Time (Minutes)',
+                                border: OutlineInputBorder()),
+                            onSaved: (value) {
+                              taskInfo.goalTime = int.tryParse(value);
+                            },
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please enter a goal time';
+                              } else {
+                                return null;
+                              }
+                            }),
+                        SizedBox(height: 8),
+                        DropDownFormField(
+                          titleText: 'Category',
+                          hintText: 'Please choose one',
+                          value: taskInfo.category,
+                          onSaved: (value) {
+                            setState(() {
+                              taskInfo.category = value;
+                            });
+                          },
+                          validator: (value) {
+                            if (value == null) {
+                              return 'Please select a category';
+                            } else {
+                              return null;
+                            }
+                          },
+                          onChanged: (value) {
+                            setState(() {
+                              taskInfo.category = value;
+                            });
+                          },
+                          dataSource: user.tasks.categories,
+                          textField: 'id',
+                          valueField: 'value',
+                        ),
+                        SizedBox(height: 20.0),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              RaisedButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text('Cancel')),
+                              RaisedButton(
+                                  color: Colors.red,
+                                  onPressed: () async {
+                                    if (formKey.currentState.validate()) {
+                                      formKey.currentState.save();
+                                      Task newTask = Task(
+                                          id: taskInfo.id,
+                                          name: taskInfo.name,
+                                          description: taskInfo.description,
+                                          durationWork: taskInfo.durationWork,
+                                          durationBreak: taskInfo.durationBreak,
+                                          goalTime: taskInfo.goalTime,
+                                          category: taskInfo.category);
 
-                            await user.tasks.add(newTask);
-                            Navigator.of(context).pop();
-                          }
-                        },
-                        child: Text('SaveEntry')
-                      )
-                    ]
-                  )
-                ]
-              ),
-            )
-          )
-        )
-      )
-    );
+                                      await user.tasks.add(newTask);
+                                      Navigator.of(context).pop();
+                                    }
+                                  },
+                                  child: Text('SaveEntry'))
+                            ])
+                      ]),
+                )))));
   }
 }
