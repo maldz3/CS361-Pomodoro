bdec83130261fb8df2cade0d6937eec8773fbc06
Jamie Mott <jamie.c.mott@gmail.com>
2020-08-07 10:12:43 -0600

    Added basic levels UI

--- lib/screens/stats_page.dart
+++ lib/screens/stats_page.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:pomodoro/our_components.dart';
 import 'package:pomodoro/our_models.dart';
+import 'package:pomodoro/styles.dart';
 
 class StatsPage extends StatelessWidget {
+  List<String> userLevels = ['Beginner', 'Task Master', 'Pomo Beast'];
+  User user = User.getInstance();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: CustomAppBar('Stats'),
         drawer: BuildDrawer(),
         body: Builder(builder: (context) {
-          return SingleChildScrollView();
+          return Column(children: <Widget>[
+            Center(
+                child: Text('Current Level: ${userLevels[user.level]}',
+                    style: Styles.headerLarge)),
+            SingleChildScrollView(child: Column())
+          ]);
         }));
   }
 }
